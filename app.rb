558812deea98d3b81ce5ac183eb695884ffcb2cd
Veronica Head <veronica.leigh.head@gmail.com>
2018-06-29 01:44:56 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,21 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  
+  get '/' do
+    "Hello, World!"
+  end
+  
+  get '?name' do
+    "My name is Veronica"
+  end
+  
+  get '/hometown' do
+    "My hometown is PA"
+  end
+  
+  get '/favorite-song' do
+    "My favorite song is happy birthday"
+  end
+  
 end
