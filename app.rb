6ad0a6a53dc338fc47362b946eb82ca6684f858d
Veronica Head <veronica.leigh.head@gmail.com>
2018-06-29 01:44:43 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -6,5 +6,16 @@ class App < Sinatra::Base
     "Hello, World!"
   end
   
+  get '?name' do
+    "My name is Veronica"
+  end
+  
+  get '/hometown' do
+    "My hometown is PA"
+  end
+  
+  get '/favorite-song' do
+    "My favorite song is happy birthday"
+  end
   
 end
